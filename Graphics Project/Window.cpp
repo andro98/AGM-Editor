e1cc2b5dd5f388b4d1a31e2d1f0b03d802651c8f
andro98 <andrewaacodo12@gmail.com>
2019-05-04 14:37:35 +0200

    finish 3d scene

--- Graphics Project/Window.cpp
+++ Graphics Project/Window.cpp
@@ -131,8 +131,8 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd , UINT message , WPARAM wParam , LPA
 			counter = 0;
 		}*/
 		//DrawmidpointEllipse(hdc, 10, 40, 200, 100, color);
-		ellipse.drawEllipseMidPoint (hdc , 0 , 0 , 500 , 200 , color);
-		//ellipse.drawEllipsePolar (hdc , 100 , 200 , 500 , 200 , color);
+		//ellipse.drawEllipseMidPoint (hdc , 100 , 200 , 500 , 200 , color);
+		ellipse.drawEllipsePolar (hdc , 100 , 200 , 500 , 200 , color);
 		//ellipse.drawEllipseDirect (hdc , 100 , 200 , 500 , 200 , color);
 		EndPaint (hwnd , &p);
 		InvalidateRect (hwnd , NULL , FALSE);//to paint more points and remain the prev points
