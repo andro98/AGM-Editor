ab874c8983c59b4e3857e6173b3b59e8e09165db
george_IVabil <45440666+aion1@users.noreply.github.com>
2019-05-04 20:26:07 +0200

    Add files via upload

--- Graphics Project/Curves.h
+++ Graphics Project/Curves.h
@@ -1,16 +1,55 @@
 #pragma once
-#include"Point.h"
-#include<Windows.h>
-#include<math.h>
+#include "Windows.h"
+#include <vector>
+#include <cmath>
+struct Vector2
+{
+	double x, y;
+	Vector2(double a = 0, double b = 0)
+	{
+		x = a; y = b;
+	}
+};
+class Vector4
+{
+	double v[4];
+public:
+	Vector4(double a = 0, double b = 0, double c = 0, double d = 0)
+	{
+		v[0] = a; v[1] = b; v[2] = c; v[3] = d;
+	}
+	Vector4(double a[])
+	{
+		memcpy(v, a, 4 * sizeof(double));
+	}
+	double& operator[](int i)
+	{
+		return v[i];
+	}
+};
+class Matrix4
+{
+	Vector4 M[4];
+public:
+	Matrix4(double A[])
+	{
+		memcpy(M, A, 16 * sizeof(double));
+	}
+	Vector4& operator[](int i)
+	{
+		return M[i];
+	}
+};
 
 class Curves
 {
 public:
-	Curves();
-	void drawCurveBezier(HDC hdc, Point& P0, Point& P1, Point& P2, Point& P3, COLORREF color);
-	void drawCurvesHermite(HDC hdc, Point& P1, Point& T2, Point& P3, COLORREF color);
-	void drawCurvesSplines(HDC hdc, Point& P0, Point& P1, Point& P2, Point& P3);
-	~Curves();
+	Curves ();
+	Vector4 GetHermiteCoeff(double x0, double s0, double x1, double s1);
+	void drawCurveBezier (HDC hdc, Vector2& P0, Vector2& P1, Vector2& P2, Vector2& P3, int numpoints);
+	void drawCurvesHermite (HDC hdc, Vector2& P0, Vector2& T0, Vector2& P1, Vector2& T1, int numpoints);
+	void drawCurvesSplines (HDC hdc, Vector2 P[], int n, double c, int numpix);
+	~Curves ();
 };
 
 
